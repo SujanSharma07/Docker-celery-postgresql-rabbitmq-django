40e00f3bbe8133223741c52ffdf376a742231fa9
SujanSharma07 <sujansharma202@gmail.com>
2022-04-28 18:10:54 +0545

    swarm script added

--- swarm.sh
+++ swarm.sh
@@ -5,7 +5,7 @@ PROJ_NAME=$PROJ_NAME
 echo $GHCR_PAT
 echo $GHCR_USERNAME
 echo $PROJ_NAME
-cd /opt/swarm_project 
+
 echo ${GHCR_PAT} | /usr/bin/docker login ghcr.io -u ${GHCR_USERNAME} --password-stdin
 /usr/bin/docker stack deploy --compose-file=/opt/swarm_project/docker-compose.yml ${PROJ_NAME}
 /usr/bin/docker service scale ${secrets.PROJ_NAME}=3
